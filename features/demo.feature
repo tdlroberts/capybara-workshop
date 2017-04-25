5792dd85ab64c00dd1c83bf7382877a83e9f5622
ulvisgoldbergs <ulvis.goldbergs@testdevlab.com>
2017-04-25 18:27:02 +0300

    2. Lecture updated

--- features/demo.feature
+++ features/demo.feature
@@ -3,3 +3,7 @@ Feature: Demo Feature
     Scenario: Open SignUp Page
         Given I am on Appimation home page
         When I click Try Now
+        And I enter demo@somethng.com in SignUp email
+        And I enter parole112 in SignUp passwords
+        And I enter demoProject in SignUp project name
+        Then I cancel SignUp
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,23 +1,28 @@
 When(/^I am on Appimation home page/) do
   visit('/')
-  
+  find(:id, 'start_button').visible?
+  find(:id, 'login-b').visible?
+  find(:id, 'signup-b').visible?
 end
 
 Then(/^I click Try Now/) do
-  
-end
-Then(/^I enter (.*) in SignUp email/) do
-  
+  find(:id, 'start_button').click
 end
 
-Then(/^I enter (.*) in SignUp passwords/) do
-  
+Then(/^I enter (.*) in SignUp email/) do |email|
+  find(:xpath, '//div[@id = "signup"]/descendant::input[@name = "login"]').send_keys email
 end
 
-Then(/^I enter (.*) in SignUp project name/) do
-  
+Then(/^I enter (.*) in SignUp passwords/) do |password|
+  find(:xpath, '//div[@id = "signup"]/descendant::input[@name = "password1"]').send_keys password
+  find(:xpath, '//div[@id = "signup"]/descendant::input[@name = "password2"]').send_keys password
+end
+
+Then(/^I enter (.*) in SignUp project name/) do |name|
+  find(:xpath, '//div[@id = "signup"]/descendant::input[@name = "project_name"]').send_keys name
 end
 
 Then(/^I cancel SignUp/) do
+  find(:xpath, '//div[@id = "signup"]/descendant::img[@class = "closecross"]').click
   
 end
